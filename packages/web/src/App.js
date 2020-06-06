11e0176c6462a2e419eebc7bb4eb351d4714890d
Saad Ibrahim <saad.lakhany@gmail.com>
2020-06-06 16:25:58 +0400

    initialize web and make web project work from common

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "@universal/common/store";
 
-function App() {
+import Home from "@universal/common/containers/Home";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Home />
+    </Provider>
   );
 }
-
-export default App;
